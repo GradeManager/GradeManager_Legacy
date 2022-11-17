56fa0264259003aa774b8bd043e0160bcfe5e1c9
unknown <megakuul@gmx.ch>
2022-11-17 22:51:29 +0100

    first commit

---
+++ gradecalculator/mainWindow.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace gradecalculator
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subjectBx = new System.Windows.Forms.Panel();
+            this.gradeList = new System.Windows.Forms.ListView();
+            this.percentHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gradeHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.nameHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // subjectBx
+            // 
+            this.subjectBx.AutoScroll = true;
+            this.subjectBx.Location = new System.Drawing.Point(12, 12);
+            this.subjectBx.Name = "subjectBx";
+            this.subjectBx.Size = new System.Drawing.Size(217, 460);
+            this.subjectBx.TabIndex = 1;
+            // 
+            // gradeList
+            // 
+            this.gradeList.BackColor = System.Drawing.Color.White;
+            this.gradeList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameHeader,
+            this.gradeHeader,
+            this.percentHeader});
+            this.gradeList.HideSelection = false;
+            this.gradeList.Location = new System.Drawing.Point(426, 12);
+            this.gradeList.Name = "gradeList";
+            this.gradeList.Scrollable = false;
+            this.gradeList.Size = new System.Drawing.Size(428, 460);
+            this.gradeList.TabIndex = 2;
+            this.gradeList.UseCompatibleStateImageBehavior = false;
+            this.gradeList.View = System.Windows.Forms.View.Details;
+            // 
+            // percentHeader
+            // 
+            this.percentHeader.Text = "Percentage";
+            // 
+            // gradeHeader
+            // 
+            this.gradeHeader.Text = "Grade";
+            // 
+            // nameHeader
+            // 
+            this.nameHeader.Text = "Exam";
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(866, 484);
+            this.Controls.Add(this.gradeList);
+            this.Controls.Add(this.subjectBx);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "mainWindow";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainWindow_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel subjectBx;
+        private System.Windows.Forms.ListView gradeList;
+        private System.Windows.Forms.ColumnHeader percentHeader;
+        private System.Windows.Forms.ColumnHeader gradeHeader;
+        private System.Windows.Forms.ColumnHeader nameHeader;
+    }
+}
+
---
+++ gradecalculator/mainWindow.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace gradecalculator
+{
+    public partial class mainWindow : Form
+    {
+        
+        public static JObject file { get; set; }
+        public static JObject Jsubjects { get; set; }
+        public static List<Subject> subjectButtons { get; set; } = new List<Subject>();
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainWindow_Load(object sender, EventArgs e)
+        {
+            gradeHeader.Width = 100;
+            percentHeader.Width = 100;
+            nameHeader.Width = gradeList.Width - percentHeader.Width - gradeHeader.Width;
+
+            if (!loadJsonConfig())
+                Application.Exit();
+        }
+
+        private bool loadJsonConfig()
+        {
+            if (!File.Exists(GetDirectoryFromExecutable() + "\\config.json"))
+            {
+                MessageBox.Show("cannot find config.json file\r\n\r\nApplication is terminated", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            
+            using (StreamReader sr = new StreamReader(GetDirectoryFromExecutable() + "\\config.json"))
+            {
+                //Init JSON variables
+                string json = sr.ReadToEnd();
+                file = JObject.Parse(json);
+                Jsubjects = (JObject)file["subjects"];
+
+                //first init the subjects
+                foreach (var item in Jsubjects)
+                {
+                    createSubButton(item.Key, subjectButtons.Count);
+                }
+            }
+            return true;
+        }
+
+        private void createSubButton(string Subject, int index)
+        {
+            Subject subject = new Subject(Subject);
+            subject.Text = Subject;
+            subject.Location = new Point(10, 50 * index + 20);
+            subject.Width = 150;
+            subject.Height = 40;
+            subject.FlatStyle = FlatStyle.Flat;
+            subject.ForeColor = Color.White;
+            subject.BackColor = Color.Gray;
+
+
+            subject.Click += (object sender, EventArgs e) =>
+            {
+                
+            };
+
+            subjectButtons.Add(subject);
+            subjectBx.Controls.Add(subject);
+        }
+
+        private void removeSubButton(string Subject)
+        {
+            foreach (Subject subject in subjectButtons)
+            {
+                if (subject.Text == Subject)
+                {
+                    subjectButtons.Remove(subject);
+                    subjectBx.Controls.Remove(subject);
+                }
+            }
+        }
+
+        private string GetDirectoryFromExecutable()
+        {
+            return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+    }
+
+    public class Subject : Button
+    {
+        public struct Exam
+        {
+            public string examname { get; set; }
+            public double grade { get; set; }
+            public int percentage { get; set; }
+        }
+
+        public List<Exam> exames { get; set; }
+
+        public Subject(string subject)
+        {
+            exames = new List<Exam>();
+
+            Exam exam = new Exam();
+
+            foreach (var item in (JObject)mainWindow.Jsubjects[subject])
+            {
+                foreach(var item2 in (JObject)item.Value)
+                {
+                    MessageBox.Show(item2.Key);
+                }
+            }
+
+
+        }
+    }
+}
